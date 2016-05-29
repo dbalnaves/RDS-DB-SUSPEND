bb407be6ef9a75a9c2e03e5110f34e3de2c74bdc
EC2 Default User <ec2-user@ip-172-31-7-37.ap-southeast-2.compute.internal>
2016-05-29 13:30:54 +0000

    Initial commit

---
+++ db-start.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python
+
+import json, sys
+import subprocess,os
+import jmespath
+
+databases=["data-test"]
+bucket="data-db-suspend"
+snapshot_suffix="-nightly"
+role="arn:aws:iam::696969696969:role/DATA-DB-SUSPEND"
+
+bucket_path="s3://"+bucket+"/"
+
+response=subprocess.check_output(["/usr/bin/aws","sts","assume-role","--role-arn",role,"--role-session-name","DATA-DB-SUSPEND"])
+
+credentials=jmespath.search("Credentials.[AccessKeyId,SecretAccessKey,SessionToken]",json.loads(response))
+credentials_env=os.environ
+credentials_env["AWS_ACCESS_KEY_ID"]=credentials[0]
+credentials_env["AWS_SECRET_ACCESS_KEY"]=credentials[1]
+credentials_env["AWS_SECURITY_TOKEN"]=credentials[2]
+
+try:
+    process=subprocess.Popen(["/usr/bin/aws","s3api","list-objects","--bucket",bucket], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=credentials_env)
+    response,error=process.communicate()
+except subprocess.CalledProcessError as error:
+    print "ERROR: Failed to list \""+bucket_path+"\":", error.output
+    sys.exit(1)
+
+db_state_list=jmespath.search("Contents[].Key",json.loads(response))
+for db_state in db_state_list:
+    db_meta_path=bucket_path+db_state
+    try:
+        process=subprocess.Popen(["/usr/bin/aws","s3","cp",db_meta_path,"-"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=credentials_env)
+        response,error=process.communicate()
+    except subprocess.CalledProcessError as error:
+        print "ERROR: Failed reading db state from \""+db_meta_path+"\":", error.output
+        sys.exit(1)
+
+    db_meta=jmespath.search("[0]",json.loads(response))
+
+    try:
+        process=subprocess.Popen(["/usr/bin/aws","rds","restore-db-instance-from-db-snapshot","--db-snapshot-identifier",db_state,"--db-instance-identifier",db_meta["DBInstanceIdentifier"]], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=credentials_env)
+        response,error=process.communicate()
+    except subprocess.CalledProcessError as error:
+        print "ERROR: Failed to restore db \""+db_meta["DBInstanceIdentifier"]+"\":", error.output
+        sys.exit(1) 
+
+    try:
+        process=subprocess.Popen(["/usr/bin/aws","rds","wait","db-instance-available","--db-instance-identifier",db_meta["DBInstanceIdentifier"]],env=credentials_env)
+        response,error=process.communicate()
+    except subprocess.CalledProcessError as error:
+        print "ERROR: Failed to enter wait while creating db instance \""+db_meta["DBInstanceIdentifier"]+"\":", error.output
+        sys.exit(1)
+
+    try:
+        process=subprocess.Popen(["/usr/bin/aws","rds","delete-db-snapshot","--db-snapshot-identifier",db_state],env=credentials_env)
+        response,error=process.communicate()
+    except subprocess.CalledProcessError as error:
+        print "ERROR: Failed to db state snapshot \""+db_state+"\":", error.output
+        sys.exit(1)
+
+    db_meta_path=bucket_path+db_meta["DBInstanceIdentifier"]+snapshot_suffix
+    try:
+        process=subprocess.Popen(["/usr/bin/aws","s3","rm",db_meta_path,"-"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=credentials_env)
+        response,error=process.communicate()
+    except subprocess.CalledProcessError as error:
+        print "ERROR: Failed to delete db state from \""+db_meta_path+"\":", error.output
+        sys.exit(1)
+    
+print "INFO: Restore db instane \""+"DBInstanceIdentifier"+"\" complete."
+sys.exit(0)
---
+++ db-stop.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+
+import json, sys
+import subprocess,os
+import jmespath
+
+databases=["data-test"]
+bucket="data-db-suspend"
+snapshot_suffix="-nightly"
+role="arn:aws:iam::696969696969:role/DATA-DB-SUSPEND"
+
+bucket_path="s3://"+bucket+"/"
+
+response=subprocess.check_output(["/usr/bin/aws","sts","assume-role","--role-arn",role,"--role-session-name","DATA-DB-SUSPEND"])
+
+credentials=jmespath.search("Credentials.[AccessKeyId,SecretAccessKey,SessionToken]",json.loads(response))
+credentials_env=os.environ
+credentials_env["AWS_ACCESS_KEY_ID"]=credentials[0]
+credentials_env["AWS_SECRET_ACCESS_KEY"]=credentials[1]
+credentials_env["AWS_SECURITY_TOKEN"]=credentials[2]
+
+try:
+    process=subprocess.Popen(["/usr/bin/aws","rds","describe-db-instances"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=credentials_env)
+    response,error=process.communicate()
+except subprocess.CalledProcessError as error:
+    print "ERROR: Query RDS instances failed:", error.output
+    sys.exit(1)
+
+all_db_meta=json.loads(response)
+
+db_meta_list=[]
+for db_name in databases:
+    db_meta_list.append(jmespath.search("DBInstances[?DBInstanceIdentifier=='"+db_name+"']",all_db_meta))
+
+if len(db_meta_list):
+    try:
+        response=subprocess.check_output(["/usr/bin/aws","s3","ls",bucket_path],stderr=subprocess.STDOUT, env=credentials_env)
+    except subprocess.CalledProcessError as error:
+        if 'NoSuchBucket' in error.output:
+            print "ERROR: Bucket \""+bucket+"\" does not exist."
+            sys.exit(1)
+        
+for db_meta in db_meta_list:
+    db_meta_path=bucket_path+db_meta[0]["DBInstanceIdentifier"]+snapshot_suffix
+    try:
+        process=subprocess.Popen(["/usr/bin/aws","s3","cp","-",db_meta_path], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=credentials_env)
+        response,error=process.communicate(json.dumps(db_meta))
+    except subprocess.CalledProcessError as error:
+        print "ERROR: Failed writing db state to \""+db_meta_path+"\":", error.output
+        sys.exit(1)
+
+    try:
+        process=subprocess.Popen(["/usr/bin/aws","rds","delete-db-instance","--no-skip-final-snapshot","--db-instance-identifier",db_meta[0]["DBInstanceIdentifier"],"--final-db-snapshot-identifier",db_meta[0]["DBInstanceIdentifier"]+snapshot_suffix], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=credentials_env)
+        response,error=process.communicate()
+    except subprocess.CalledProcessError as error:
+        print "ERROR: Failed to terminate db instance \""+db_meta[0]["DBInstanceIdentifier"]+"\":", error.output
+        sys.exit(1)
+
+    try:
+        process=subprocess.Popen(["/usr/bin/aws","rds","wait","db-instance-deleted","--db-instance-identifier",db_meta[0]["DBInstanceIdentifier"]],env=credentials_env)
+        response,error=process.communicate()
+    except subprocess.CalledProcessError as error:
+        print "ERROR: Failed to enter wait while terminating db instance \""+db_meta[0]["DBInstanceIdentifier"]+"\":", error.output
+        sys.exit(1)
+
+print "INFO: Suspend db instane \""+db_meta[0]["DBInstanceIdentifier"]+"\" complete."
+sys.exit(0)
